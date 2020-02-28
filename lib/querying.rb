a0e4cf17091ac08175a7a736e5219a556621a43b
Agnes Park <agnespark824@gmail.com>
2020-02-28 02:13:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters
+  "SELECT species, COUNT(species) AS count_species FROM characters
+    GROUP BY species
     ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
